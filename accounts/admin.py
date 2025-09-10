42f7fff8dd11146abf362271db1b995928926c68
georgievG3 <156510257+georgievG3@users.noreply.github.com>
2025-09-10 18:45:00 +0300

    admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from accounts.models import AppUser
+
+
 # Register your models here.
+@admin.register(AppUser)
+class AppUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name', 'is_staff')
--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from reservations.models import Reservation
+
+
 # Register your models here.
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'phone', 'email', 'people', 'start_date', 'end_date', 'status', 'notes', 'created_at')
+    list_filter = ('name', 'surname', 'phone', 'email', 'people', 'start_date', 'end_date', 'status', 'notes', 'created_at')
+    search_fields = ('name', 'surname', 'phone', 'email', 'people', 'start_date', 'end_date', 'status', 'notes', 'created_at')
+    ordering = ('-created_at',)
+
